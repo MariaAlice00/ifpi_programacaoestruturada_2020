42bb557db605eabb50a4fa5bd2ba2b61d35eab93
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-28 16:58:55 -0300

    Update variavel_multipla_escolha.py

--- quiz/variavel_multipla_escolha.py
+++ quiz/variavel_multipla_escolha.py
@@ -1,17 +1,21 @@
-print('''
-Q1 - No Python, como se chama uma 'caixa' usada para armazenar dados?
-a - texto
-b - variavel
-c - uma caixa de sapatos
-''')
-resposta = input().lower()
+def main():
+    print('''
+    Q1 - No Python, como se chama uma 'caixa' usada para armazenar dados?
+    a - texto
+    b - variavel
+    c - uma caixa de sapatos
+    ''')
+    resposta = input().lower()
 
-if resposta == "a":
-    print(" Não - texto é um tipo de dado :( ")
-elif resposta == "b":
-    print(" Correto!! :) ")
-elif resposta == "c":
-    print(" Não seja bobinho! :( ")
-else:
-    print(" Você não escolheu a, b ou c :( ")
-    
+    if resposta == "a":
+        print(" Não - texto é um tipo de dado :( ")
+    elif resposta == "b":
+        print(" Correto!! :) ")
+    elif resposta == "c":
+        print(" Não seja bobinho! :( ")
+    else:
+        print(" Você não escolheu a, b ou c :( ")
+        
+
+if __name__ == "__main__":
+    main()
