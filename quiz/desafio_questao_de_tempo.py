c7eaf19f697dfb296598370aa6ca944859492e9f
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-28 16:54:54 -0300

    Update desafio_questao_de_tempo.py

--- quiz/desafio_questao_de_tempo.py
+++ quiz/desafio_questao_de_tempo.py
@@ -1,10 +1,18 @@
 from time import sleep
-print('Vou pensar em um número entre 0 e 10.')
-sleep(1)
-print('Eu pensei em qual número?')
-numero = input('>>> ')
-if numero == '5':
-    print('Parabéns!!! Você acertou.')
-else:
-    print('Você errou. Que pena.')
-print('Obrigada por jogar!')
+
+def main():
+    print('Vou pensar em um número entre 0 e 10.')
+    sleep(1)
+    print('Eu pensei em qual número?')
+    
+    numero = input('>>> ')
+    
+    if numero == '5':
+        print('Parabéns!!! Você acertou.')
+    else:
+        print('Você errou. Que pena.')
+    print('Obrigada por jogar!')
+
+
+if __name__ == "__main__":
+    main()
