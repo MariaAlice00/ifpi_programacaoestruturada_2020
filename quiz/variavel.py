86e5a200a5c5911a7d6d3a5946a0585044aba701
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-28 16:57:28 -0300

    Update variavel.py

--- quiz/variavel.py
+++ quiz/variavel.py
@@ -1,9 +1,14 @@
-print("No Python, como se chama uma 'caixa' usada para armazenar dados?")
-resposta = input().lower()
+def main():
+    print("No Python, como se chama uma 'caixa' usada para armazenar dados?")
+    resposta = input().lower()
 
-if resposta == "variavel":
-    print(" :) " * 100)
-else:
-    print(" :( " * 100)
+    if resposta == "variavel":
+        print(" :) " * 100)
+    else:
+        print(" :( " * 100)
 
-print("Obrigada por jogar!")
+    print("Obrigada por jogar!")
+
+
+if __name__ == "__main__":
+    main()
