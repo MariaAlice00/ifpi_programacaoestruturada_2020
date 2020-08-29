480e323e7f07922ff5d6d4ebd877574f78b46758
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-29 09:45:51 -0300

    Update 01.py

--- s3_t1/01.py
+++ s3_t1/01.py
@@ -1,15 +1,20 @@
 '''Escreva um programa que leia o nome e o sexo de uma pessoa, e mostre o nome precedido da mensagem “Ilmo Sr.”, caso seja informado o sexo masculino, ou “Ilma Sra.” 
 se for informado o sexo feminino. Use o número inteiro 1 para identificar masculino e 2 para identificar feminino.'''
 
+def masculino(sexo):
+    return sexo == 1
+
+def feminino(sexo):
+    return sexo == 2
+
 def main():
     nome = str(input())
     sexo = int(input())
 
-    if sexo == 1:
+    if masculino(sexo):
         print('Ilmo Sr. ' + nome)
-    elif sexo == 2:
+    elif feminino(sexo):
         print('Ilma Sra. ' + nome)
-
 
 if __name__ == "__main__":
     main()
