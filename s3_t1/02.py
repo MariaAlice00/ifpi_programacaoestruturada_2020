1f3f3a19aa142f149f20388c3e850f7f5e523553
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-29 10:05:05 -0300

    Update 02.py

--- s3_t1/02.py
+++ s3_t1/02.py
@@ -1,12 +1,15 @@
 '''Escreva um programa que leia um número e mostra o valor booleano True (verdadeiro) se o número for ímpar ou o valor booleano False (falso) caso contrário.'''
 
+def impar(num):
+    return num % 2 != 0
+
 def main():
     num = int(input())
-    if num % 2 != 0:
+    
+    if impar(num):
         print('True')
     else:
         print('False')
-
 
 if __name__ == "__main__":
     main()
