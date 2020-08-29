395f8ee033723ec02da5c1d2dbe1408956ac9587
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-29 10:56:37 -0300

    Update 05.py

--- s3_t2/05.py
+++ s3_t2/05.py
@@ -1,16 +1,25 @@
 '''Escreva um programa que leia um caractere e mostra uma das mensagens: “vogal”, “consoante”, “número” ou “símbolo”. Observação: O cedilha “ç”, caracteres acentuados, espaço em branco e outros como “símbolo”;'''
 
+def vogal(valor):
+    return valor in 'aeiou'    
+
+def consoante(valor):
+    return valor in 'bcdfghjklmnpqrstvwxyz'
+
+def numero(valor):
+    return valor in '0123456789'
+
 def main():
     valor = input().lower()
-    if valor in 'aeiou':
+
+    if vogal(valor):
         print('vogal')
-    elif valor in 'bcdfghjklmnpqrstvwxyz':
+    elif consoante(valor):
         print('consoante')
-    elif valor in '0123456789':
+    elif numero(valor):
         print('número')
     else:
         print('símbolo')
-
 
 if __name__ == "__main__":
     main()
