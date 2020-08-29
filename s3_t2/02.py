4557d9e3087a4e5d1d227e7c0241ad7eaa14e8d0
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-29 10:47:05 -0300

    Update 02.py

--- s3_t2/02.py
+++ s3_t2/02.py
@@ -1,12 +1,15 @@
 ''' Escreva um programa que leia um caractere e mostra o valor booleano True (verdadeiro) se for uma letra (vogal ou consoante) ou o valor booleano False (falso) caso contrário.'''
 
+def letra(valor):
+    return valor in 'abcdefghijklmnopqrstuvwxyz'
+
 def main():
     valor = input().lower()
-    if valor in 'abcdefghijklmnopqrstuvwxyz':
+    
+    if letra(valor):
         print('True')
     else:
-        print('False')
-
+        print('False')   
 
 if __name__ == "__main__":
     main()
