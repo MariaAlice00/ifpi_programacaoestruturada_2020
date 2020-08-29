319f8abc4dbe6f053b1f4aec37c5dde50272ab9d
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-29 10:50:15 -0300

    Update 03.py

--- s3_t2/03.py
+++ s3_t2/03.py
@@ -1,12 +1,15 @@
 '''Escreva um programa que leia um caractere e mostra o valor booleano True (verdadeiro) se for uma consoante ou o valor booleano False (falso) caso contrário.'''
 
+def consoante(valor):
+    return valor in 'bcdfghjklmnpqrstvwxyz'    
+
 def main():
     valor = input().lower()
-    if valor in 'bcdfghjklmnpqrstvwxyz':
+
+    if consoante(valor):
         print('True')
     else:
         print('False')
 
-
 if __name__ == "__main__":
-    main()
+    main())
