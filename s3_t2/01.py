5107b331f6f55b6d8e275cf513c85eeeff589e33
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-29 10:40:39 -0300

    Update 01.py

--- s3_t2/01.py
+++ s3_t2/01.py
@@ -1,12 +1,15 @@
 '''Escreva um programa que leia um caractere e mostra o valor booleano True (verdadeiro) se for uma vogal ou o valor booleano False (falso) caso contrário.'''
 
+def vogal(valor):
+    return valor in 'aeiou'
+
 def main():
     valor = input().lower()
-    if valor in ('a', 'e', 'i', 'o', 'u'):
+    
+    if vogal(valor):
         print('True')
     else:
-        print('False')
-
+        print('False')   
 
 if __name__ == "__main__":
     main()
