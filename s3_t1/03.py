b81233eebc987a83afbc33822f01c3e515265fab
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-08-29 10:13:07 -0300

    Update 03.py

--- s3_t1/03.py
+++ s3_t1/03.py
@@ -1,14 +1,23 @@
 '''Escreva um programa que leia a cor de um sinal de trânsito (“V” é verde; “A” é amarelo; “E” é vermelho) e retorne a respectiva mensagem “Siga”, “Atenção”, ou “Pare”. Assuma entradas válidas.'''
 
+def verde(cor):
+    return cor == 'V'
+
+def amarelo(cor):
+    return cor == 'A'
+
+def vermelho(cor):
+    return cor == 'E'
+
 def main():
     cor = str(input()).upper()
-    if cor == 'V':
+
+    if verde(cor):
         print('Siga')
-    elif cor == 'A':
+    elif amarelo(cor):
         print('Atenção')
-    elif cor == 'E':
+    elif vermelho(cor):
         print('Pare')
-
-
+                    
 if __name__ == "__main__":
     main()
