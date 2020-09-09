d3b14265d85c1346962006823278b3bacf019c75
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-09-09 20:18:55 -0300

    Add files via upload

---
+++ s5_t2/04.py
@@ -0,0 +1,9 @@
+def main():
+    for c in range(99, 0, -11):
+        print(f'{c} bugs no software, pegue onze deles e conserte...')
+        print('Tecle "Ctrl+F5"')
+    print('Sem erros no software! Está estabilizado!')
+
+
+if __name__ == "__main__":
+    main()
