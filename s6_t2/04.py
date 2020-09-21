aaa0cba76a20e6132baa0e1bd306569fe9029752
MariaAlice00 <61831459+MariaAlice00@users.noreply.github.com>
2020-09-20 21:36:49 -0300

    Update 04.py

--- s6_t2/04.py
+++ s6_t2/04.py
@@ -1,17 +1,41 @@
-'''erro'''
-
 def main():
-    num = int(input())
+    while True:
+        nota = float(input())
 
-    inverso(num)
+        if nota <= 10 and nota >= 0:
+            if nota_a(nota) == True:
+                print('A')
+            elif nota_b(nota) == True:
+                print('B')
+            elif nota_c(nota) == True:
+                print('C')
+            elif nota_d(nota) == True:
+                print('D')
+            elif nota_e(nota) == True:
+                print('E')
+            break
+        else:
+            print('Nota inválida.')
+        
+
+def nota_a(nota):
+    return nota >= 8.5
 
 
-def inverso(num):
-    y = 1
-    while num / y >= 1:
-        x = num // y % 10
-        y *= 10
-        print(x, end='')
+def nota_b(nota):
+    return nota >= 7
+
+
+def nota_c(nota):
+    return nota >= 5
+
+
+def nota_d(nota):
+    return nota >= 4
+
+
+def nota_e(nota):
+    return nota >= 0
 
 
 if __name__ == "__main__":
